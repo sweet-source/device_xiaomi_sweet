d7d54e8224ffcd6dbb385fe5aebd2af23da8014d
afterallafk <shivamatiet2001@gmail.com>
2024-04-07 16:38:27 +0530

    sweet: Setup vendorsetup.sh
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/afterallafk/device_xiaomi_sm6150-common.git -b 14 --depth=1 device/xiaomi/sm6150-common
+git clone https://github.com/xiaomi-sm6150/proprietary_vendor_xiaomi_sweet.git -b lineage-21 --depth=1 vendor/xiaomi/sweet
+git clone https://github.com/xiaomi-sm6150/proprietary_vendor_xiaomi_sm6150-common.git -b lineage-21 --depth=1 vendor/xiaomi/sm6150-common
+git clone https://github.com/unmoved21/kernel_xiaomi_sweet.git -b fourteen --depth=1 kernel/xiaomi/sm6150
