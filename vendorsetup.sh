6a853e9f8de52d6b294620ceb190d3f941ded23e
afterallafk <shivamatiet2001@gmail.com>
2024-06-11 17:22:19 +0530

    sweet: Update vendorsetup
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/afterallafk/device_xiaomi_sm6150-common.git -b 14 --depth=1 device/xiaomi/sm6150-common
 git clone https://github.com/xiaomi-sm6150/proprietary_vendor_xiaomi_sweet.git -b lineage-21 --depth=1 vendor/xiaomi/sweet
 git clone https://github.com/xiaomi-sm6150/proprietary_vendor_xiaomi_sm6150-common.git -b lineage-21 --depth=1 vendor/xiaomi/sm6150-common
-git clone https://github.com/unmoved21/kernel_xiaomi_sweet.git -b fourteen --depth=1 kernel/xiaomi/sm6150
-git clone https://github.com/PixelOS-Devices/vendor_xiaomi_sweet-miuicamera.git -b fourteen vendor/xiaomi/sweet-miuicamera
+git clone https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git -b sleepy-inline --depth=1 kernel/xiaomi/sm6150
+git clone https://gitlab.com/unmoved21/sweet-miuicamera.git -b leica-holybear vendor/xiaomi/sweet-miuicamera
