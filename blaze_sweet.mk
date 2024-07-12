3091a4276ba62071a7802f7bbdd53b695bd6ae34
afterallafk <shivamatiet2001@gmail.com>
2024-07-12 07:30:36 +0000

    sweet: Use Google Pixel 8 Pro build fingerprint
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- blaze_sweet.mk
+++ blaze_sweet.mk
@@ -29,6 +29,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Redmi/sweet_global/sweet:13/TKQ1.221013.002/V14.0.9.0.TKFMIXM:user/release-keys
+BUILD_FINGERPRINT := google/husky/husky:14/AP2A.240705.005/11942872:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sweet_global-user 13 TKQ1.221013.002 V14.0.9.0.TKFMIXM release-keys"
+    PRIVATE_BUILD_DESC="husky-user 14 AP2A.240705.005 11942872 release-keys"
