84308e95ef0f2bc1fb7a777d13578a06e2e0c552
kamleshhv <kamleshvansajalia@gmail.com>
2024-09-29 05:25:48 +0000

    switch to vantom kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/sweet-source/device_xiaomi_sm6150-common.git -b test device/xiaomi/sm6150-common
 git clone https://github.com/sweet-source/vendor_xiaomi_sweet.git -b test vendor/xiaomi/sweet
 git clone https://github.com/sweet-source/vendor_xiaomi_sm6150-common.git -b test vendor/xiaomi/sm6150-common
-git clone https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git -b sleepy --depth=1 kernel/xiaomi/sm6150
+git clone https://github.com/PixelOS-Devices/kernel_xiaomi_sm6150.git -b fourteen --depth=1 kernel/xiaomi/sm6150
 git clone https://github.com/PixelOS-Devices/vendor_xiaomi_sweet-miuicamera.git -b fourteen vendor/xiaomi/sweet-miuicamera
